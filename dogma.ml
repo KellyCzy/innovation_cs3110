922bbc4c301aaf125aa4800bc7b0defde1d31d3e
Haocheng Shen <hs668@cornell.edu>
2019-11-02 02:02:21 -0400

    update game.ml and dogma.ml

--- dogma.ml
+++ dogma.ml
@@ -12,7 +12,7 @@ type effect =
   | Splay of splay_direction
   | Return of int
   | Score of int
-  | Transfer of ard_pile  il
-)  | Demand of int * effect list
+  | Transfer of card_pile * card_pile
+  | Demand of int * effect list
 
 type t = effect list
