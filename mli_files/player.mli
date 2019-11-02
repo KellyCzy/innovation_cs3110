76f1bf574bb92f1895aef5b336dfbb94728b5bfe
Ruiqi Zhu <rz327@cornell.edu>
2019-11-01 23:30:56 -0400

    Update player.mli

--- mli_files/player.mli
+++ mli_files/player.mli
@@ -1,45 +1,54 @@
 open Card
+open Dogma
+
+type stack
 
 type t
 
-(** [get_hand t] is a set-list list of hand cards of player [t]. *)
+(** [get_hand t] is the list of hand cards of [t]. *)
 val get_hand : t -> Card.t list
 
+(** [add_hand t c] is the list of hand cards of [t] 
+    after adding [c]. *)
+val add_hand : t -> Card.t -> Card.t list
 
-(** [post_hand t c d] is a set-like list of hand cards of player [t],
-    after the card [c] is moved to/away from hand, the direction of which 
-    is specified by [d]. *)
-val set_hand : t -> Card.t -> bool -> Card.t list
+(** [remove_hand t i] is the list of hand cards of [t] 
+    after removing the card with index [i]. *)
+val remove_hand : t -> int -> Card.t list
 
-(** [get_board t] is a set-like list of board cards of player [t]. *)
-val get_board : t -> Card.t list
+(** [get_stack t i] is the list of cards of [t]'s stack 
+    with index of [i]. *)
+val get_stack : t -> int -> Card.t list
 
-(** [post_board t c d] is a set-like list of board cards of player [t],
-    after the card [c] is moved to/away from board, the direction of which 
-    is specified by [d]. *)
-val set_board : t -> Card.t -> bool -> Card.t list
+(** [add_stack t i_h] is the list of cards of [t]'s
+    stack with the same color as the card with index of 
+    [i_h] from [t]'s hand cards, after adding that card.
+    Create a new stack if there is no such stack. ] *)
+val add_stack : t -> int -> Card.t -> Card.t list
 
-(** [get_score t] is the current score of player [t]. *)
+(** [remove_stack c i] is the list of cards of [t]'s
+    stack with color [c] after removing its [n]th 
+    card. *)
+val remove_stack : t -> Dogma.stack_color -> int -> Card.t list
+
+(** [get_score t] is the current score of [t]. *)
 val get_score : t -> int
 
-(** [post_score t x d] is the current score of player [t], after [x] points
-    are added to/subtracted from score, the direction of which is specified
-    by [d]. *)
-val set_score : t -> int -> bool -> int
+(** [add_score t x] is the current score of [t] after
+    adding [x]. *)
+val add_score : t -> int -> int
 
-(** [get_achievement t] is a set-like list of achievements of player [t]. *)
+(** [remove_score t x] is the current score of [t] after
+    removing [x]. *)
+val remove_score : t -> int -> int
+
+(** [get_achievement t] is the list of achievements of [t]. *)
 val get_achievements : t -> int list
 
-(** [post_achievement t a d] is a set-like list of achievements of player [t], 
-    after the achievement [a] is placed/removed, the direction of which is 
-    specified by [d]. *)
-val set_achievements : t -> int -> bool -> int list
+(** [add_achievement t e] is the list of achievements of [t]
+    after adding the achievement represented by (in) [e] *)
+val add_achievement : t -> int -> int list
 
-(** [meld t a] is a set-like list of board cards of player [t]. *)
-val meld : t -> bool -> Card.t list
-(** [draw t a] is a set-like list of board cards of player [t]. *)
-val draw : t -> Card.t list
-val achieve : t -> Achievement.t list
-val dogma : t -> bool -> State.t
-val first_action : t -> bool -> State.t
-val second_action : t -> bool -> State.t
+(** [remove_achievement t e] is the list of achievements of [t]
+    after removing the achievement represented by (in) [e]*)
+val remove_achievement : t -> int -> int list
