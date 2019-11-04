eacbd08ef869c61490bb5e27498c4476ef88affa
zc96 <zc96@cornell.edu>
2019-11-04 15:54:22 -0500

    main

--- main.ml
+++ main.ml
@@ -7,8 +7,8 @@ open Command
 let total_era = 1
 
 let game_init f =
-  f |> Yojson.Basic.from_file |> game.all_cards total_era
-  |> State.init_state 
+  let json = f |> Yojson.Basic.from_file in 
+  Game.all_cards json total_era |> State.init_state 
 
 
 let win (state: State.t): bool = 
@@ -18,7 +18,7 @@ let win (state: State.t): bool =
 
 
 (** Helper function *)
-let run_game_1 state = 
+let run_game_1 game state = 
   if state |> win then (print_string ("Game ends!"); 
                         print_string "\n"; exit 0)
   else
@@ -34,12 +34,12 @@ let run_game_1 state =
       print_string "No such command! \n";
       print_string "You can only Meld/Draw/Dogma/Achieve \n";
       run_game_1 game state
-    | Meld x -> let new = State.meld state state.current_player x 
-      in run_game_2 new;
-    | Draw x -> let new = State.draw state state.current_player x 
-      in run_game_2 new;
-    | Achieve _ -> let new = State.achieve state state.current_player
-      in run_game_2 new;
+    | Meld x -> let new_state = State.meld state state.current_player x 
+      in run_game_2 new_state;
+    | Draw x -> let new_state = State.draw state state.current_player x 
+      in run_game_2 new_state;
+    | Achieve _ -> let new_state = State.achieve state state.current_player
+      in run_game_2 new_state;
     | Dogma col -> 
       let num = Player.map_color_to_int col in
       let stack = Player.get_ith_stack (State.current_player state) num in
@@ -55,12 +55,12 @@ let run_game_1 state =
 
 let dogma_effect state dogma = begin
   match dogma with
-  | Draw x -> let new = State.draw state state.current_player x 
-    in run_game_2 new;
-  | Meld x -> let new = State.meld state state.current_player x 
-    in run_game_2 new;
-  | Tuck x -> let new = State.tuck state state.current_player x 
-    in run_game_2 new;
+  | Draw x -> let new_state = State.draw state state.current_player x 
+    in run_game_2 new_state;
+  | Meld x -> let new_state = State.meld state state.current_player x 
+    in run_game_2 new_state;
+  | Tuck x -> let new_state = State.tuck state state.current_player x 
+    in run_game_2 new_state;
   | _ -> print_string "Need to be completed \n";
 end
 
@@ -71,7 +71,7 @@ end
 
 let play_game f =
   let state = game_init f in (
-    run_game state
+    run_game_1 state
   )
 
 (** [main ()] prompts for the game to play, then starts it. *)
