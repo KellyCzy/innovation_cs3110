35c6c0f5375e31b0c336f4e4c0431524a4716aa6
Haocheng Shen <hs668@cornell.edu>
2019-11-19 23:09:19 -0500

    update frontend

--- frontend.ml
+++ frontend.ml
@@ -7,17 +7,30 @@ let get_score state =
   (State.get_score_by_id state ((idx + 2) mod 4)) ::
   (State.get_score_by_id state ((idx + 3) mod 4)) :: []
 
+let hand state id = 
+  State.get_hand_size_by_id state id 
+
+let score state id = 
+  State.get_score_by_id state id
+
 let display state = 
+  let current_id = State.get_current_player state in
+  let player2 = (current_id + 1) mod 4 in 
+  let player3 = (current_id + 2) mod 4 in 
+  let player4 = (current_id + 3) mod 4 in
   print_string " ---------------------------------------------------- \n";
-  print_string ("|          " ^ "player: " ^ (string_of_int (((State.get_current_player state) + 2) mod 4)) ^ "                                 | \n");
-  print_string ("|          " ^ "score: " ^ (string_of_int (List.nth (get_score state) 2)) ^ "                                  | \n");
+  print_string ("|            " ^ "player: " ^ (string_of_int player3) ^ "                               | \n");
+  print_string ("|            " ^ "score: " ^ (string_of_int (score state player3)) ^ "                                | \n");
+  print_string ("|            " ^ "hand: " ^ (string_of_int (hand state player3)) ^ "                                 | \n");
   print_string "|                                                    | \n";
   print_string "|                                                    | \n";
-  print_string ("| " ^ "player: " ^ (string_of_int (((State.get_current_player state) + 3) mod 4)) ^ "                           " ^ "player: " ^ (string_of_int (((State.get_current_player state) + 1) mod 4)) ^ "      | \n");
-  print_string ("| " ^ "score: " ^ (string_of_int (List.nth (get_score state) 3)) ^ "                             " ^ "score: " ^ (string_of_int (List.nth (get_score state) 1)) ^ "      | \n");
+  print_string ("| " ^ "player: " ^ (string_of_int player4) ^ "                           " ^ "player: " ^ (string_of_int player2) ^ "      | \n");
+  print_string ("| " ^ "score: " ^ (string_of_int (score state player4)) ^ "                             " ^ "score: " ^ (string_of_int (score state player2)) ^ "      | \n");
+  print_string ("| " ^ "hand: " ^ (string_of_int (hand state player4)) ^ "                               " ^ "hand: " ^ (string_of_int (hand state player2)) ^ "      | \n");
   print_string "|                                                    | \n";
   print_string "|                                                    | \n";
-  print_string ("|            " ^ "player: " ^ (string_of_int (State.get_current_player state)) ^ "                               | \n");
-  print_string ("|            " ^ "score: " ^  (string_of_int (List.nth (get_score state) 0)) ^ "                                | \n");
+  print_string ("|            " ^ "player: " ^ (string_of_int current_id) ^ "                               | \n");
+  print_string ("|            " ^ "score: " ^  (string_of_int (score state current_id)) ^ "                                | \n");
+  print_string ("|            " ^ "hand: " ^  (string_of_int (hand state current_id)) ^ "                                 | \n");
   print_string " ----------------------------------------------------- \n";
 
